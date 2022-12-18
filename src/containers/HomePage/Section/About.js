a871581d5be4b91f990efd4d98cbbdc2b0b4e2f8
datnguyen5121 <datnguyen5121@gmail.com>
2022-12-18 11:43:18 +0700

    helloworld

---
+++ src/containers/HomePage/Section/About.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+
+class About extends Component {
+
+    render() {
+        return (
+            <div className='section-share section-about'>
+               <div className='section-about-header'>
+                    Truyền thông nói gì về NguyenDat
+               </div>
+               <div className='section-about-content'>
+                    <div className='content-left'>
+                        <iframe width="100%" height="400px" 
+                            src="https://www.youtube.com/embed/147SkAVXEqM?list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI" 
+                            title="#51 Kết Thúc Design Giao Diện Clone BookingCare.vn 4 | React.JS Cho Người Mới Bắt Đầu" 
+                            frameborder="0" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allowfullscreen>
+                        </iframe>
+                    </div>
+                    <div className='content-right'>
+                        <p>
+                        JavaScript is the programming language of the Web. JavaScript is easy to learn. This tutorial will teach you JavaScript from basic to advanced
+                        </p>
+                    </div>
+               </div>
+            </div>
+
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+        //injex
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
