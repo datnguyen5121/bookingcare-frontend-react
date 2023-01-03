8d2ccbe3a6fcc04209bd6ccab8064e6d27354b5a
datnguyen5121 <datnguyen5121@gmail.com>
2023-01-03 18:51:32 +0700

    helo

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,51 +1,59 @@
 export const adminMenu = [
-    { //quản lý người dùng
-        name: 'menu.admin.manage-user', 
+    {
+        //quản lý người dùng
+        name: "menu.admin.manage-user",
         menus: [
             {
-                name: 'menu.admin.crud', link: '/system/user-manage'
+                name: "menu.admin.crud",
+                link: "/system/user-manage",
             },
             {
-                name: 'menu.admin.crud-redux', link: '/system/user-redux'
+                name: "menu.admin.crud-redux",
+                link: "/system/user-redux",
             },
 
             {
-                name: 'menu.admin.manage-doctor', link: '/system/user-doctor'
+                name: "menu.admin.manage-doctor",
+                link: "/system/manage-doctor",
                 // subMenus: [
                 //     { name: 'menu.system.system-administrator.user-manage', link: '/system/user-manage' },
                 //     { name: 'menu.system.system-administrator.user-redux', link: '/system/user-redux' },
                 // ]
             },
             {
-                name: 'menu.admin.manage-admin', link: '/system/user-admin'
+                name: "menu.admin.manage-admin",
+                link: "/system/user-admin",
             },
-           
-        ]
+        ],
     },
-    { //quản lý phòng khám
-        name: 'menu.admin.clinic', 
+    {
+        //quản lý phòng khám
+        name: "menu.admin.clinic",
         menus: [
             {
-                name: 'menu.admin.manage-clinic', link: '/system/manage-clinic'
+                name: "menu.admin.manage-clinic",
+                link: "/system/manage-clinic",
             },
-           
-        ]
+        ],
     },
-    { //quản lý chuyên khoa
-        name: 'menu.admin.specialty', 
+    {
+        //quản lý chuyên khoa
+        name: "menu.admin.specialty",
         menus: [
             {
-                name: 'menu.admin.manage-specialty', link: '/system/manage-specialty'
+                name: "menu.admin.manage-specialty",
+                link: "/system/manage-specialty",
             },
-        ]
+        ],
     },
-    { //quản lý cẩm nang
-        name: 'menu.admin.handbook', 
+    {
+        //quản lý cẩm nang
+        name: "menu.admin.handbook",
         menus: [
             {
-                name: 'menu.admin.manage-handbook', link: '/system/manage-handbook'
+                name: "menu.admin.manage-handbook",
+                link: "/system/manage-handbook",
             },
-           
-        ]
+        ],
     },
 ];
