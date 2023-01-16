8f6061b5e927fcb0fb8619d4cf8064b26351509e
datnguyen5121 <datnguyen5121@gmail.com>
2023-01-16 22:49:13 +0700

    helo

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -10,6 +10,7 @@ import * as actions from "../../../../store/actions";
 import { LANGUAGES } from "../../../../utils";
 import Select from "react-select";
 import { postPatientBookAppointment } from "../../../../services/userService";
+import moment from "moment";
 import { toast } from "react-toastify";
 class BookingModal extends Component {
     constructor(props) {
@@ -83,8 +84,40 @@ class BookingModal extends Component {
     handleChangeSelect = (selectedOption) => {
         this.setState({ selectedGender: selectedOption });
     };
+    buildTimeBooking = (dataTime) => {
+        let { language } = this.props;
+        console.log("renderTimeBooking dataTime", dataTime);
+        if (dataTime && !_.isEmpty(dataTime)) {
+            let time =
+                language === LANGUAGES.VI
+                    ? dataTime.timeTypeData.valueVi
+                    : dataTime.timeTypeData.valueEn;
+            let date =
+                language === LANGUAGES.VI
+                    ? moment.unix(+dataTime.date / 1000).format("dddd - DD/MM/YYYY")
+                    : moment
+                          .unix(+dataTime.date / 1000)
+                          .locale("en")
+                          .format("ddd - MM/DD/YYYY");
+            return `${time} - ${date}`;
+        }
+        return "";
+    };
+    buildDoctorName = (dataTime) => {
+        let { language } = this.props;
+        if (dataTime && !_.isEmpty(dataTime)) {
+            let name =
+                language === LANGUAGES.VI
+                    ? `${dataTime.doctorData.lastName} ${dataTime.doctorData.firstName}`
+                    : `${dataTime.doctorData.firstName} ${dataTime.doctorData.lastName}`;
+            return name;
+        }
+        return "";
+    };
     handleConfirmBooking = async () => {
         let date = new Date(this.state.birthday).getTime();
+        let timeString = this.buildTimeBooking(this.props.dataTime);
+        let doctorName = this.buildDoctorName(this.props.dataTime);
         let res = await postPatientBookAppointment({
             fullName: this.state.fullName,
             phoneNumber: this.state.phoneNumber,
@@ -95,6 +128,9 @@ class BookingModal extends Component {
             selectedGender: this.state.selectedGender.value,
             doctorId: this.state.doctorId,
             timeType: this.state.timeType,
+            language: this.props.language,
+            timeString: timeString,
+            doctorName: doctorName,
         });
         if (res && res.errCode === 0) {
             toast.success("Booking a new appointment succeed!");
--- src/services/userService.js
+++ src/services/userService.js
@@ -50,6 +50,9 @@ const getProfileDoctorById = (doctorId) => {
 const postPatientBookAppointment = (data) => {
     return axios.post("/api/patient-book-appointment", data);
 };
+const postVerifyBookAppointment = (data) => {
+    return axios.post("/api/verify-book-appointment", data);
+};
 export {
     handleLoginApi,
     getAllUsers,
@@ -66,4 +69,5 @@ export {
     getExtraInforDoctorById,
     getProfileDoctorById,
     postPatientBookAppointment,
+    postVerifyBookAppointment,
 };
